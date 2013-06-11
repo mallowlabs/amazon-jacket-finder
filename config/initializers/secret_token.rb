d6d073b26c59fadfa5c73b80654e0d63ab09dfae
mallowlabs <mallowlabs@gmail.com>
2013-06-11 13:21:27 +0900

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArtWorkFinder::Application.config.secret_token = '9a8a476d2c78990baa6d2d7737f3f6350c0d9a372e8837121311f5ea4df632f9fea4ba5bd610530d16fabe9c48e52f5d005f3b32d88a24f9106c60c5dc2cde0d'
