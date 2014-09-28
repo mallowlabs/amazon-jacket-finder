3233324c742b7db7789feeb580956bf2f1feeb7b
mallowlabs <mallowlabs@gmail.com>
2014-09-28 22:07:36 +0900

    Added: error alert

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,9 +6,16 @@ class SearchController < ApplicationController
     @query = params['keyword']
     @page = (params['page'] || '1')
 
-    @results = Amazon::Ecs.item_search(@query, :response_group => 'Medium',
-                                               :search_index => 'Music',
-                                               :country => 'jp',
-                                               :item_page => @page)
+    @items = []
+    begin
+      @items = Amazon::Ecs.item_search(@query, :response_group => 'Medium',
+                                                 :search_index => 'Music',
+                                                 :country => 'jp',
+                                                 :item_page => @page).items
+    rescue => e
+      logger.error(e.message)
+      logger.error(e.backtrace.join("\n"))
+      flash[:error] = e.message
+    end
   end
 end
