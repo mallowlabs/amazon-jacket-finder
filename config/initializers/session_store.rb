3ea781c9e267f50911ba4690920e248a03d26569
mallowlabs <mallowlabs@gmail.com>
2014-09-28 21:53:05 +0900

    Fixed: session store key

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-AmazonJacketFinder::Application.config.session_store :cookie_store, key: '_art-work-finder_session'
+AmazonJacketFinder::Application.config.session_store :cookie_store, key: '_amazon_jacket_finder_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
